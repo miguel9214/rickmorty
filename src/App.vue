b606766888d589a079718e5fe540ec3b84cbfc6f
Miguel Ramos <miguel921433@gmail.com>
2023-12-17 20:02:24 -0500

    consumo de api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1 class="title">Consultar datos</h1>
+  </div>
+  <button v-on:click="fecth">Consultar</button>
+  <div v-for="personaje of personajes" v-bind:key="personaje.id">
+    {{ personaje.id }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      personajes: [],
+    };
+  },
+  methods: {
+    fecth() {
+      let result = axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then((res) => {
+          this.personajes = res.data.results;
+        });
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
