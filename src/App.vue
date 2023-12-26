e3693f0e4fd20f48ee70a173a139bbe35fa7206b
Miguel Ramos <miguel921433@gmail.com>
2023-12-26 11:53:20 -0500

    pagination realizada

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,20 @@
     </div>
     <br />
     <div class="container">
-      <div
-        class="columns is-desktop is-mobile is-tablet is-multiline is-centered"
-      >
-        <Character
-          v-for="character of characters"
-          v-bind:key="character.id"
-          v-bind:character="character"
-        />
+      <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
+        <Character v-for="character of characters" v-bind:key="character.id" v-bind:character="character" />
       </div>
+      <nav class="pagination" role="navigation" aria-label="pagination">
+        <a class="pagination-previous" v-on:click="changedPage(page - 1)">Anterior</a>
+        <ul class="pagination-list ">
+          <li>
+            <a href="" class="pagination-link is-current">{{ page }}</a>
+          </li>
+        </ul>
+        <a class="pagination-next" v-on:click="changedPage(page + 1)">Siguiente</a>
+      </nav>
     </div>
+
   </div>
 </template>
 
@@ -36,22 +40,34 @@ export default {
   data: function () {
     return {
       characters: [],
+      page: 1,
+      pages: 1,
     };
   },
-
   created() {
     this.fecth();
   },
   methods: {
+
     fecth() {
+
+      const params = {
+        page: this.page
+      }
+
       let result = axios
-        .get("https://rickandmortyapi.com/api/character")
+        .get("https://rickandmortyapi.com/api/character/", { params })
         .then((res) => {
           this.characters = res.data.results;
+          this.pages = res.data.info.pages;
         });
     },
+    changedPage(page) {
+      this.page = (page <= 0 || page > this.pages) ? this.page : page;
+      this.fecth();
+    }
   },
+
 };
 </script>
 
-<style></style>
