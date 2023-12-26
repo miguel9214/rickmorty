5b65afbb253729a016e716cd6c097596e0fefb0b
Miguel Ramos <miguel921433@gmail.com>
2023-12-25 21:35:24 -0500

    division de componentes con vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,53 @@
 <template>
   <div id="app">
-    <h1 class="title">Consultar datos</h1>
-  </div>
-  <button v-on:click="fecth">Consultar</button>
-  <div v-for="personaje of personajes" v-bind:key="personaje.id">
-    {{ personaje.id }}
+    <div class="hero is-white is-gradient is-bold">
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">R&M</span>
+          <span class="subtitle">Personajes</span>
+        </h1>
+        <button class="button is-success is-rounded" v-on:click="fecth">
+          Consultar
+        </button>
+      </div>
+    </div>
+    <br />
+    <div class="container">
+      <div
+        class="columns is-desktop is-mobile is-tablet is-multiline is-centered"
+      >
+        <Character
+          v-for="character of characters"
+          v-bind:key="character.id"
+          v-bind:character="character"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Character from "./components/Character";
 import axios from "axios";
 
 export default {
+  components: { Character },
   name: "App",
   data: function () {
     return {
-      personajes: [],
+      characters: [],
     };
+  },
+
+  created() {
+    this.fecth();
   },
   methods: {
     fecth() {
       let result = axios
         .get("https://rickandmortyapi.com/api/character")
         .then((res) => {
-          this.personajes = res.data.results;
+          this.characters = res.data.results;
         });
     },
   },
---
+++ src/components/Character.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="columns is-12-mobile is-4-desktop is-4-tablet"
+    v-bind-key="character.id">
+    <div class="card">
+      <div class="card-header">
+        <img v-bind:src="character.image" v-bind:alt="character.name" />
+      </div>
+      <div class="card-content">
+        <h3 class="title is-size-4">{{ character.name }}</h3>
+        <button class="button is-success is-rounded is-small">Ver mas</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["character"],
+};
+</script>
